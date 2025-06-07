a22504b15eebb72c1531b1a85f7eeab27d1c1493
ecthon <ecthon@gmail.com>
2025-06-06 21:48:08 -0400

    feat: Adiciona dependências @tanstack/react-query e axios, implementa ReactQueryProvider no layout e utiliza useQuery para buscar filmes na página inicial.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
+'use client'
+import { api } from "@/api/axios";
 import { Button } from "@/components/ui/button";
+import { useQuery } from '@tanstack/react-query';
 
 export default function Home() {
+  const { data: movies, isLoading, error } = useQuery({
+    queryKey: ['movies'],
+    queryFn: async () => {
+      const response = await api.get('/movies');
+      console.log(response.data);
+      return response.data;
+    }
+  });
+
+  if (isLoading) return <div>Carregando...</div>;
+  if (error) return <div>Erro ao buscar filmes</div>;
+
   return (
-    <div><Button>Hello</Button></div>
+    <div>
+      {movies && movies.map && movies.map((movie: any) => (
+        <div key={movie.id}>{movie.title}</div>
+      ))}
+      <Button>Hello</Button>
+    </div>
   );
 }
